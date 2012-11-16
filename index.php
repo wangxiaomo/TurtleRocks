874a799ffb12b973a521ab81b0a6ddd037c2c576
xiaomo <wxm4ever@gmail.com>
2012-11-16 22:07:39 +0800

    add ie6 debug

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-header("Location:src/index.php");
+if((strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== FALSE)){
+  header("Location:ie6/index.html");
+}else{
+  header("Location:src/index.php");
+}
 ?>
